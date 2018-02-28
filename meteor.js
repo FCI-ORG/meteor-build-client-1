45ee98bad0ec97223b41d51811bf5cb3a38e6b96
Anjani Kumar <AnjaniKumar15@users.noreply.github.com>
2018-02-28 18:59:28 +0530

    Allowed default DDP connection url

--- meteor.js
+++ meteor.js
@@ -169,8 +169,8 @@ module.exports = {
             scripts = scripts.replace('__meteor_runtime_config__', '<script type="text/javascript">__meteor_runtime_config__ = JSON.parse(decodeURIComponent("'+encodeURIComponent(JSON.stringify(settings))+'"));</script>');
             
             // add Meteor.disconnect() when no server is given
-            if(!program.url)
-                scripts += '        <script type="text/javascript">Meteor.disconnect();</script>';
+            //if(!program.url)
+             //   scripts += '        <script type="text/javascript">Meteor.disconnect();</script>';
 
             content = content.replace(/{{ *> *scripts *}}/, scripts);
 
